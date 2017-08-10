96bbc631a71b720e8d867dce5a704ccb809330c5
Kevin Peña <kevin.pena.prog@gmail.com>
2017-08-09 23:21:37 -0500

    Add icons to the actions

--- src/views/Actions.js
+++ src/views/Actions.js
@@ -4,7 +4,12 @@ import PlusIcon from 'material-ui/svg-icons/content/add'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import { List, ListItem } from 'material-ui/List'
+import ArrowUp from 'material-ui/svg-icons/navigation/arrow-upward'
+import ArrowDown from 'material-ui/svg-icons/navigation/arrow-downward'
+import Cross from 'material-ui/svg-icons/navigation/close'
+import Check from 'material-ui/svg-icons/navigation/subdirectory-arrow-right'
 import { fetchJson } from '../utils'
+
 
 const styles = {
   fab: {
@@ -17,6 +22,12 @@ const styles = {
     maxWidth: 'none',
   },
 }
+
+const gray = 'rgba(0, 0, 0, 0.6)'
+const red = 'red'
+const blue = 'blue'
+const green = 'green'
+const orange = 'orange'
 
 class Actions extends Component {
   state = {
@@ -64,21 +75,58 @@ class Actions extends Component {
 
   genActionText = action => {
     const triggers = this.state.actionNames[action.triggerId]
-    
+
     switch (action.type) {
     case 'enable':
       return `Habilitar acción ${triggers}` + this.genCondText(action)
+    case 'disable':
+      return `Deshabilitar acción ${triggers}` + this.genCondText(action)
     case 'sell':
-      return `Vender a ${action.value}`
+      return `Vender a ${action.value}` + this.genCondText(action)
+    case 'buy':
+      return `Vender a ${action.value}` + this.genCondText(action)
     default:
       return `Acción inválida`
+    }
+  }
+
+  genIcon = action => {
+    const common = {
+      style: {
+        width: '36px',
+        height: '36px',
+      }
+    }
+    switch (action.type) {
+    case 'sell':
+    {
+      const color = action.enabled ? red : gray
+      return <ArrowDown {...common} color={color} />
+    }
+    case 'buy':
+    {
+      const color = action.enabled ? green : gray
+      return <ArrowUp {...common} color={color} />
+    }
+    case 'enable':
+    {
+      const color = action.enabled ? blue : gray
+      return <Check {...common} color={color} />
+    }
+    case 'disable':
+    {
+      const color = action.enabled ? orange : gray
+      return <Cross {...common} color={color} />
+    }
+    default:
+      return null
     }
   }
 
   renderAction = (action, i) => {
     const style = {}
     if (!action.enabled)
-      style.color = 'rgba(0, 0, 0, 0.6)'
+      style.color = gray
     return (
       <ListItem
         key={i}
@@ -86,6 +134,7 @@ class Actions extends Component {
         primaryText={action.name + (action.enabled ?' (Habilitada)' : '')}
         secondaryText={this.genActionText(action)}
         secondaryTextLines={2}
+        leftIcon={this.genIcon(action)}
       />
     )
   }
