b1fba14c677d8959b91d548ee0fb0ce9a2d5d937
Kevin Peña <kevin.pena.prog@gmail.com>
2017-08-15 21:23:20 -0500

    Allow to register to the server for push notifications

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
-import { urlBase64ToUint8Array } from '../utils'
+import { urlBase64ToUint8Array, fetchJson } from '../utils'
 
 const SUBSCRIBE_PUBLIC_KEY = process.env.REACT_APP_SUBSCRIBE_PUBLIC_KEY
 
@@ -8,7 +8,9 @@ class Home extends Component {
   static isPrivate = true
 
   state = {
-    notificationMessage: ''
+    notificationMessage: '',
+    subscribed: false,
+    disableNotifications: false,
   }
 
   componentWillMount = async () => {
@@ -31,6 +33,8 @@ class Home extends Component {
         notificationMessage: 'Las notificaciones están activadas'
       })
     }
+
+    this.updateSubscribeButton()
   }
 
   handleLogout = () => {
@@ -38,11 +42,26 @@ class Home extends Component {
     this.props.history.push('/')
   }
 
+  updateSubscribeButton = async () => {
+    const {isSubscribed} =
+      await fetchJson('/subscriptions/isSubscribed', this.props.auth)
+
+    const permission = await Notification.requestPermission()
+    if (permission !== 'granted')
+      this.setState({disableNotifications: true})
+
+    this.setState({subscribed: isSubscribed})
+  }
+
   setupPush = async () => {
+    // TODO: Allow to unregister from push
     const registration = this.props.sw
     // Ask permission to show notifications
     // UX will be handled on the Home view
-    await Notification.requestPermission()
+    const permission = await Notification.requestPermission()
+    if (permission !== 'granted')
+      return this.setState({disableNotifications: true})
+
     const subscribeOptions = {
       userVisibleOnly: true,
       applicationServerKey: urlBase64ToUint8Array(SUBSCRIBE_PUBLIC_KEY)
@@ -51,16 +70,30 @@ class Home extends Component {
     const pushSubscription =
       await registration.pushManager.subscribe(subscribeOptions)
 
-    console.log(pushSubscription)
+    await fetchJson('/subscriptions/register', this.props.auth, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        subscription: JSON.stringify(pushSubscription)
+      })
+    })
+
+    this.updateSubscribeButton()
   }
 
   render () {
+    const buttonText = this.state.disableNotifications ?
+      'Notificaciones no permitidas' :
+      (this.state.subscribed ?
+        'Dejar de recibir notificaciones' :
+        'Registrarse para recibir notificaciones')
     return (
       <div>
         <h1>Action Trader</h1>
         <main>
           <p>Eventos recientes o algo asi iria aqui</p>
-          <p>{this.state.notificationMessage}</p>
           <RaisedButton
             label='Cerrar sesión'
             primary={true}
@@ -68,7 +101,8 @@ class Home extends Component {
           />
           <br />
           <RaisedButton
-            label='Registrarse para recibir notificaciones'
+            label={buttonText}
+            disabled={this.state.disableNotifications}
             primary={true}
             onClick={this.setupPush}
           />
