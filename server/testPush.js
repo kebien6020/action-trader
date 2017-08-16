bd08bab5ff2c4a29c4c47adb605c25bc0f53e53c
Kevin Peña <kevin.pena.prog@gmail.com>
2017-08-16 12:44:32 -0500

    Change test notification text

--- server/testPush.js
+++ server/testPush.js
@@ -4,7 +4,7 @@ const { async, await } = require('asyncawait')
 module.exports = async ((req, res, next) => {
   try {
     const userId = req.user.sub
-    await (push(userId, 'Hola desde el server x2'))
+    await (push(userId, 'Notificacion push de prueba'))
 
     res.json({success: true})
   } catch (err) { next(err) }
