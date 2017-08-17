419b1901b581c2315bf75a8ef556d4eb231686e3
Kevin Peña <kevin.pena.prog@gmail.com>
2017-08-17 13:07:26 -0500

    Pass props to view through AuthRoute

--- src/AuthRoute.js
+++ src/AuthRoute.js
@@ -5,14 +5,14 @@ const auth  = new Auth()
 
 const isAuthenticated = () => auth.isAuthenticated()
 
-const AuthRoute = ({component, sw, ...props}) => {
+const AuthRoute = ({component, sw, ...outerProps}) => {
   const { isPrivate } = component
 
   if (isAuthenticated() || !isPrivate) {
     // Authenticated users have access to every route
     const Component = component
-    const render = (props) => <Component auth={auth} sw={sw} {...props} />
-    return <Route { ...props } render={render} />
+    const render = (props) => <Component auth={auth} sw={sw} {...outerProps} {...props} />
+    return <Route { ...outerProps } render={render} />
   } else {
     // User is not Authenticated and route is not private
     auth.login()
