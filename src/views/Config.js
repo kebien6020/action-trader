25ec2f56e7e334dd9a8164fbfc362739f0e56ffb
Kevin Peña <kevin.pena.prog@gmail.com>
2017-08-28 14:26:31 -0500

    Add settings main menu

--- src/views/Config.js
+++ src/views/Config.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import { List, ListItem } from 'material-ui/List'
+import Divider from 'material-ui/Divider'
+import NotificationIcon from 'material-ui/svg-icons/social/notifications'
+import PoloniexIcon from '../icons/PoloniexIcon'
+
+import theme from '../theme'
 
 class Config extends Component {
   componentWillMount() {
     this.props.onMount({menu: null})
   }
-  
+
   render() {
     return (
-      <h1>Config view</h1>
+      <List>
+        <Link to='/config/notifications'>
+          <ListItem
+            leftIcon={<NotificationIcon
+              color={theme.palette.accent1Color}
+            />}
+            primaryText='Notificaciones'
+          />
+        </Link>
+        <Divider />
+        <Link to='/config/poloniex'>
+          <ListItem
+            leftIcon={<PoloniexIcon
+              color={theme.palette.accent1Color}
+            />}
+            primaryText='Poloniex'
+          />
+        </Link>
+      </List>
     )
   }
 }
