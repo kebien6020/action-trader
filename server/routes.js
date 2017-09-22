1d9539c13334a47f39991fc208246ff1d96ec7a9
Kevin Peña <kevin.pena.prog@gmail.com>
2017-09-22 14:49:53 -0500

    partially refactor and test router

--- server/routes.js
+++ server/routes.js
@@ -1,32 +1,24 @@
-const Router = require('express').Router
-const actionHandlers = require('./actions')
+import { Router } from 'express'
+import * as actionHandlers from './actions'
+import * as configHandlers from './config'
 const subscriptionHandlers = require('./subscriptions')
 const testPushHandler = require('./testPush')
-const configHandlers = require('./config')
 
-const actions = Router()
-
+export const actions = Router()
 actions.get('/', actionHandlers.list)
 actions.get('/:id', actionHandlers.detail)
 actions.put('/:id', actionHandlers.update)
 actions.post('/', actionHandlers.create)
 actions.delete('/:id', actionHandlers.del)
 
-exports.actions = actions
-
-const subscriptions = Router()
-
+export const subscriptions = Router()
 subscriptions.post('/register', subscriptionHandlers.register)
 subscriptions.post('/unregister', subscriptionHandlers.unregister)
 subscriptions.get('/isSubscribed', subscriptionHandlers.isSubscribed)
 
-exports.subscriptions = subscriptions
-
-const testPush = Router()
+export const testPush = Router()
 testPush.get('/', testPushHandler)
-exports.testPush = testPush
 
-const config = Router()
+export const config = Router()
 config.get('/poloniex', configHandlers.getPoloniex)
 config.put('/poloniex', configHandlers.updatePoloniex)
-exports.config = config
---
+++ server/routes.test.js
@@ -0,0 +1,23 @@
+// import request from 'supertest'
+// import {
+//   actions,
+//   // subscriptions,
+//   // testPush,
+//   // config,
+// } from './routes'
+
+const routes = require('./routes')
+
+// import * as actionHandlers from './actions'
+
+// const request = () => {}
+
+describe('routes mapping', () => {
+  it('maps GET /actions/ to actionsHandlers.list', () => {
+    // actionHandlers.list = jest.fn()
+    // return request(actions).get('/')
+    //   .then(() => {
+    //     expect(actionHandlers.list).toHaveBeenCalledTimes(1)
+    //   })
+  })
+})
