51ab995186c9650f1efe52e38b0a3b3caabb8636
Kevin Peña <kevin.pena.prog@gmail.com>
2017-10-06 16:43:11 -0500

    Implement generate stair in terms of a closing stair
    Specifically a closing stair where the initialStopDistance is the same
    as the finalStopDistance, it has just 1 step to the final stopDistance
    and use linear interpolation

--- src/common/generatorUtils.js
+++ src/common/generatorUtils.js
@@ -18,76 +18,22 @@ export function generateStair(args) {
     amountType,
   } = args
 
-  if (direction !== Direction.UPSTAIRS && direction !== Direction.DOWNSTAIRS)
-    throw Error(
-      'direction should come from the enum-like object ' +
-      'Direction defined in generatorUtils.js'
-    )
-  const up = direction === Direction.UPSTAIRS
-
-  // Differences between Upstairs and Downstairs
-  if (!up) {
-    step = -step
-    stopDistance = -stopDistance
-    limitDelta = -limitDelta
+  const closingStairArgs = {
+    direction,
+    minValueToAction: initialValue,
+    step,
+    initialStopDistance: stopDistance,
+    finalStopDistance: stopDistance,
+    stepsToFinal: 1,
+    limitDelta,
+    stepQty,
+    existingActions,
+    amount,
+    amountType,
+    method: 'linear',
   }
 
-  const passCheck = up ? 'gt' : 'lt'
-  const returnCheck = up ? 'lt' : 'gt'
-  const finalAction = up ? 'sell' : 'buy'
-
-  // Settle on an unique prefix
-  const prefix = definePrefix(up, existingActions)
-
-  // Actual generation logic
-  const actions = []
-  for (let i = 1; i <= stepQty; ++i) {
-    const currentStepValue = initialValue + (i - 1) * step
-    const currentStopLimit = currentStepValue - stopDistance
-    const currentLimit = currentStopLimit - limitDelta
-    // Disable previous stop limit
-    if (i > 1) {
-      actions.push({
-        name: `${prefix} Paso ${i} mover stop`,
-        type: 'disable',
-        triggerName: `${prefix} Paso ${i - 1} stop`,
-        check: passCheck,
-        value: currentStepValue,
-        enabled: true,
-      })
-    }
-    // Enable current stop limit
-    actions.push({
-      name: `${prefix} Paso ${i} stop stop`,
-      type: 'enable',
-      triggerName: `${prefix} Paso ${i} stop`,
-      check: passCheck,
-      value: currentStepValue,
-      enabled: true,
-    })
-
-    // Stop limit
-    actions.push({
-      name: `${prefix} Paso ${i} stop`,
-      type: 'enable',
-      triggerName: `${prefix} Paso ${i} limit`,
-      check: returnCheck,
-      value: currentStopLimit,
-      enabled: false,
-    })
-
-    // Limit
-    actions.push({
-      name: `${prefix} Paso ${i} limit`,
-      type: finalAction,
-      value: currentLimit,
-      enabled: false,
-      amount,
-      amountType,
-    })
-  }
-
-  return actions
+  return generateClosingStair(closingStairArgs)
 }
 
 export function definePrefix(sell, existingActions) {
